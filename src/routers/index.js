e1fce606cc85938501ac000592ab782bdc92f186
QuanK1905 <caovanquan.qv@gmail.com>
2022-06-26 05:50:29 +0700

    login + register

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,12 +2,15 @@ import Home from '../pages/Home'
 import Product from '../pages/Product'
 import Login from '../pages/Login'
 import User from '../pages/User'
+import Register from '../pages/Register'
+
 
 const router = [
-    { path: '/', component: Home },
+    { path: '/', component: Login },
     { path: '/product', component: Product },
     { path: '/login', component: Login },
     { path: '/user', component: User },
+    { path: '/register', component: Register },
 ]
 
 export { router }

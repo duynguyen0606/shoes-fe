82385325439885948530734fb24538d1bf5ff11a
QuanK1905 <caovanquan.qv@gmail.com>
2022-06-28 17:36:03 +0700

    update admin

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,14 +3,16 @@ import Product from '../pages/Product'
 import Login from '../pages/Login'
 import User from '../pages/User'
 import Register from '../pages/Register'
+import Admin from '../pages/Admin'
 
 
 const router = [
-    { path: '/', component: Login },
+    { path: '/', component: Admin },
     { path: '/product', component: Product },
     { path: '/login', component: Login },
     { path: '/user', component: User },
     { path: '/register', component: Register },
+    { path: '/register', component: Admin },
 ]
 
 export { router }

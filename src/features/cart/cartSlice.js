d40e29283d265ea6063eebe733fe72f6673a592a
manhdat <tangdatsdbg@gmail.com>
2022-07-07 09:07:32 +0700

    logic cart by bac giang

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,12 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    products: []
+    products: [
+
+    ]
 }
+
+const storeLocal = (cart) => {
+    localStorage.removeItem('cart');
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
-    reducers:{
+    reducers: {
         setCart: (state, action) => {
             state.products = action.payload;
         },
@@ -15,19 +23,22 @@ const cartSlice = createSlice({
                 ...state.products,
                 action.payload
             ];
+            storeLocal(state);
         },
         deleteProductInCart: (state, action) => {
             state.products = state.products.filter(product => product._id !== action.payload._id);
+            storeLocal(state);
         },
         updateCart: (state, action) => {
             state.products = state.products.map(product => {
-                if(product._id === action.payload._id){
+                if (product._id === action.payload._id) {
                     product.amount = product.amount + action.payload.amount;
                     return product;
-                }else{
+                } else {
                     return product;
                 }
             })
+            storeLocal(state);
         }
     }
 })
